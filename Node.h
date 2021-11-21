3b217c300848cd7e53c208d575e322da881bf570
oaikenhead <oaikenhead@nevada.unr.edu>
2021-11-21 15:48:08 -0800

    Adding the initial file setup

---
+++ Node.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+
+struct RequestCheck {
+  char origin_RREQ;
+  int ID_REQ_RREQ;
+};
+
+class Node {
+  public:
+
+  private:
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+ *
+ *
+ */
+#include "Node.h"
+
+int main() {
+  std::cout << "CPE 400 Project" << std::endl;
+  
+  return 0;
+}
