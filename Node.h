637be6ec55a77d1d1399d95deb8932d6b5282a2c
oaikenhead <oaikenhead@nevada.unr.edu>
2021-11-25 10:18:21 -0800

    Wrote function defs and complied

--- Node.h
+++ Node.h
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <time.h>
+#include <map>
 
 struct RequestCheck {
   char origin_RREQ;
@@ -9,6 +11,59 @@ struct RequestCheck {
 
 class Node {
   public:
+    Node(char nodeLetter, std::map<char, Node*> connects);
+    Node& operator=(const Node& other);
+    Node* memLocation();
+    void connectAssign(std::map<char, Node*> connects);
+    void insConnect(char nodeDes, Node* loc);
+    void removeConnect(char nodeDes);
+    void sendRREP(char src, char dest, int size, std::string RREQ_reply, char RREP_SRC);
+    bool connected(char nodeName);
 
-  private:
+    std::map<char, Node*> nodeConnects;
+    std::vector<RequestCheck> prevReq;
+    char nodeName;
+    std::string RREQ_string;
+    std::string RREP_string;
+    bool pathRec;
+    bool responseRec;
 };
+
+Node::Node(char nodeLetter, std::map<char, Node*> connects) {
+  // setting node designation to letter
+  nodeName = nodeLetter;
+
+  // creating Node connections
+  connectAssign(connects);
+
+  // setting path and response recieved to false
+  pathRec = false;
+  responseRec = false;
+}
+
+Node& Node::operator=(const Node& rhs) {
+  if (this == &rhs) {
+    return *this;
+  }
+  return *this;
+}
+
+Node* Node::memLocation() {
+  return this;
+}
+
+void Node::connectAssign(std::map<char, Node*> connects) {
+  nodeConnects = connects;
+}
+
+void Node::insConnect(char nodeDes, Node* loc) {
+  nodeConnects.insert(std::pair<char, Node*>(nodeDes, loc));
+}
+
+void Node::removeConnect(char nodeDes) {
+  nodeConnects.erase(nodeDes);
+}
+
+/*void sendRREP(char src, char dest, int size, string RREQ_reply, char RREP_SRC) {
+
+}*/
